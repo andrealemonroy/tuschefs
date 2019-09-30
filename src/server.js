200c9c72ffec773bead4671a5909d3c33cb95e37
andrealemonroy <andreale17@icloud.com>
2019-09-30 00:36:30 -0500

    add

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/ng-tuschefs'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/ng-tuschefs/index.html'));
+app.use(express.static(__dirname/front-end/dist));
+app.get('*',(req,res) => {
+    res.sendFile(`./front-end/dist/index.html`);
 });
 
 app.listen(process.env.PORT || 8080);
