0bbc193548834027e3923a565395d109e630e2d6
andrealemonroy <andreale17@icloud.com>
2019-09-30 00:05:19 -0500

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDheJYiYDC1EmjaewhdL0EGXDLWdqyyDso",
+    authDomain: "tuschefs-1844b.firebaseapp.com",
+    databaseURL: "https://tuschefs-1844b.firebaseio.com",
+    projectId: "tuschefs-1844b",
+    storageBucket: "",
+    messagingSenderId: "1067634760884",
+    appId: "1:1067634760884:web:ef01a0dfe82eb647"
+  }
 };
